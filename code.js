113744faaf41803d0556f9e4d52d5924263597ae
Hamilton Clark <dhclark2@ncsu.edu>
2016-12-19 23:22:42 -0500

    did first 7 questions.

---
+++ code.js
@@ -0,0 +1,28 @@
+// 1. Assign the message "Hello, World!" to a variable.
+var easy = "Hello, World!";
+
+// 2. Assign a different string to a variable name of your choice.
+var different = "This is a different string.";
+
+// 3. Assign a number to a variable.
+var num = 5;
+
+// 4. Use string concatenation to display the number from #3 in a string.
+var num_different = num + " " + different;
+
+// 5. Make an array of at least four of your favorite movies or books or bands.
+var favs = ["Cryptonomicon", "Lord of the Rings", "Hook", "The Fountainhead"];
+
+// 6. Make a object of information about yourself with at least four properties.
+var person = {
+    firstname: "Hamilton",
+    lastname: "Clark",
+    age: 30,
+    haircolor: "brown"
+    eyecolor: "green"
+};
+
+// 7. Make an array of objects containing more information about your favorite movies. The objects should have at least three properties.
+var array7 = [favs.length, favs[favs.length -1], favs[0]];
+
+// 8. Use `for` to loop through the answer from exercise #7 and print only one property from the object. i.e., given `{ title: "Gone with the Wind" }` you print "Gone with the Wind".
